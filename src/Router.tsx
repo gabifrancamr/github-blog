3a4a7fbe1c75878a2b064286369402750a912aac
Gabriela França <gabifmouraroc@outlook.com>
2024-02-19 15:10:48 -0300

    configurações iniciais e header

---
+++ src/Router.tsx
@@ -0,0 +1,13 @@
+import { Route, Routes } from 'react-router-dom'
+import { Blog } from './pages/Blog'
+import { DefaultLayout } from './layout/DefaultLayout'
+
+export function Router() {
+  return (
+    <Routes>
+      <Route path="/" element={<DefaultLayout />}>
+        <Route path="/" element={<Blog />} />
+      </Route>
+    </Routes>
+  )
+}
